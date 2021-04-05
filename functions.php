9e96c9b74cabd1553ffaed4db4cd47fc50659bb6
Gaetan Janssens <janssensgaetan@gmail.com>
2021-04-05 09:56:38 +0200

    Theme init function for woocommerce

--- functions.php
+++ functions.php
@@ -14,3 +14,18 @@ add_action( 'event_tickets_after_save_ticket', function( $post_id, $ticket ) {
     $product->set_catalog_visibility( 'visible' );
     $product->save();
 }, 100, 2 );
+
+// Hook in
+add_filter( 'woocommerce_default_address_fields' , 'aveloles_override_default_address_fields' );
+
+// gjanssens : Do not autofill field needed for shipping comute
+function aveloles_override_default_address_fields( $address_fields ) {
+    $address_fields['billing_postcode']['required'] = false;
+    $address_fields['billing_city']['required'] = false;
+    $address_fields['billing_country']['required'] = false;
+    $address_fields['shipping_postcode']['required'] = false;
+    $address_fields['shipping_city']['required'] = false;
+    $address_fields['shipping_country']['required'] = false;
+
+    return $address_fields;
+}
